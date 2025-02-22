0ee6c3ebe4341c7461b8a72b5d09644e7219329c
donishabduvaliyev <donishkratos413677@gmail.com>
2025-02-23 02:20:01 +0500

    fix-error-9

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-
 const connectDB = async () => {
-  try {
-    await connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
-  }
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`❌ MongoDB connection error: ${error.message}`);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
