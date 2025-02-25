b2cff7527772c1857d89292345df3f7504f13a5c
donishabduvaliyev <donishkratos413677@gmail.com>
2025-02-25 12:29:47 +0500

    fix-error-send-data-11

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ app.post("/web-data", async (req, res) => {
         console.log("📩 Received order data from frontend:", data, OWNER_CHAT_IDS);
         console.log(OWNER_CHAT_IDS);
 
-        console.log(data[0].user);
+        console.log(data[0]);
         
 
         const user = data[0].user;
